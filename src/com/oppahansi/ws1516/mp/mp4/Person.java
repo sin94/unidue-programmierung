0fade186d81e06276a22ee8c72449427ffc56435
oppahansi <alexander.schellenberg@mindcurv.com>
2015-12-12 13:11:19 +0100

    MP4 geloest

---
+++ src/com/oppahansi/ws1516/mp/mp4/Person.java
@@ -0,0 +1,64 @@
+/**
+ * Created by:
+ * Institut für Informatik und Wirtschaftsinformatik, Universität Duisburg-Essen
+ *
+ * For learning purpose only.
+ *
+ * Solved/Edited by Oppa Hansi. Possible solution - there are other ways to
+ * solve these tasks.
+ */
+
+package com.oppahansi.ws1516.mp.mp4;
+
+public class Person {
+
+   private String name;
+   private Rang rang;
+   private String rangname;
+   private boolean istmaennlich;
+   private Person linkerNf;
+   private Person rechterNf;
+
+   Person(String name, Rang rang, Person linkerNf, Person rechterNf, boolean istmaennlich) {
+      this.name = name;
+      this.rang = rang;
+      this.rangname = rang.toString();
+      this.linkerNf = linkerNf;
+      this.rechterNf = rechterNf;
+      this.istmaennlich = istmaennlich;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public Rang getRang() {
+      return rang;
+   }
+
+   public Person getLinks() {
+      return linkerNf;
+   }
+
+   public Person getRechts() {
+      return rechterNf;
+   }
+
+   public boolean getIstMaennlich() {
+      return istmaennlich;
+   }
+
+   public void setLinks(Person linkerNf) {
+      this.linkerNf = linkerNf;
+   }
+
+   public void setRechts(Person rechterNf) {
+      this.rechterNf = rechterNf;
+   }
+
+   public void vorstellen() {
+      System.out.println(
+         "Mein Name ist " + name + ", ich habe den Rang " + rangname + " und bin " + (istmaennlich ? "ein Mann" : "eine Frau") + ".");
+   }
+
+}
---
+++ src/com/oppahansi/ws1516/mp/mp4/Rang.java
@@ -0,0 +1,23 @@
+/**
+ * Created by:
+ * Institut für Informatik und Wirtschaftsinformatik, Universität Duisburg-Essen
+ *
+ * For learning purpose only.
+ *
+ * Solved/Edited by Oppa Hansi. Possible solution - there are other ways to
+ * solve these tasks.
+ */
+
+package com.oppahansi.ws1516.mp.mp4;
+
+public enum Rang {
+
+   ARBEITER,
+   BUERGER,
+   FUERST,
+   KOENIG;
+
+   public String toString() {
+      return this.name();
+   }
+}
