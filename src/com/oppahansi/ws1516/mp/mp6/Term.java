864ddc3c4dba41245c9a35e1e161d122a94ef265
Alexander Schellenberg <alexander.schellenberg@mindcurv.com>
2016-02-16 23:33:15 +0100

    MP6, Uebung 8, 9 geloest

---
+++ src/com/oppahansi/ws1516/mp/mp6/Term.java
@@ -0,0 +1,19 @@
+/**
+ * Created by:
+ * Institut für Informatik und Wirtschaftsinformatik, Universität Duisburg-Essen
+ *
+ * For learning purpose only.
+ *
+ * Solved/Edited by Oppa Hansi. Possible solution - there are other ways to
+ * solve these tasks.
+ */
+
+package com.oppahansi.ws1516.mp.mp6;
+
+/**
+ * Veranstaltungszeitraum
+ */
+public enum Term {
+  SUMMER_TERM, // Sommersemester
+  WINTER_TERM // Wintersemester
+}
---
+++ src/com/oppahansi/ws1516/uebungen/uebung08/E.java
@@ -0,0 +1,15 @@
+/**
+ * Created by:
+ * Institut für Informatik und Wirtschaftsinformatik, Universität Duisburg-Essen
+ *
+ * For learning purpose only.
+ *
+ * Solved/Edited by Oppa Hansi. Possible solution - there are other ways to
+ * solve these tasks.
+ */
+
+package com.oppahansi.ws1516.uebungen.uebung08;
+
+public class E extends C {
+
+}
