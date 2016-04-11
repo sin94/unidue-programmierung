9c0d8ad5e7a30264c641cc9d859dffcd8775d3bc
Alexander Schellenberg <alexander.schellenberg@mindcurv.com>
2016-04-11 19:01:19 +0200

    LED Projekt dateien hinzugefuegt und Placeholder für Uebungen - MPs

---
+++ src/com/oppahansi/ss16/mp/mp1/Placeholder.java
@@ -0,0 +1,8 @@
+package com.oppahansi.ss16.mp.mp1;
+
+/**
+ * Created by oppa on 11.04.2016.
+ */
+public class Placeholder {
+
+}
---
+++ src/com/oppahansi/ss16/uebungen/uebung1/Placeholder.java
@@ -0,0 +1,8 @@
+package com.oppahansi.ss16.uebungen.uebung1;
+
+/**
+ * Created by oppa on 11.04.2016.
+ */
+public class Placeholder {
+
+}
