fdade09790f8bffba90df1a797e3532f38f94753
Alexander Schellenberg <alexander.schellenberg@mindcurv.com>
2016-06-11 13:10:52 +0200

    MP3 added

---
+++ src/com/oppahansi/ss16/mp/mp3/Rang.java
@@ -0,0 +1,20 @@
+/**
+ * Created by:
+ * Institut f�r Informatik und Wirtschaftsinformatik, Universit�t Duisburg-Essen
+ * <p>
+ * For learning purpose only.
+ */
+
+package com.oppahansi.ss16.mp.mp3;
+
+public enum Rang {
+
+  ARBEITER,
+  BUERGER,
+  FUERST,
+  KOENIG;
+
+  public String toString() {
+    return this.name();
+  }
+}
