33c342f411677f3e649e3485d49996af95df0125
cadingel <cadingel@gmail.com>
2017-12-14 06:03:51 +0100

    Led projecte verschoben, doppelten code reduziert

---
+++ src/com/oppahansi/ws1718/mp/mp3/Rang.java
@@ -0,0 +1,13 @@
+package com.oppahansi.ws1718.mp.mp3;
+
+public enum Rang {
+
+    ARBEITER,
+    BUERGER,
+    FUERST,
+    KOENIG;
+
+    public String toString() {
+        return this.name();
+    }
+}
