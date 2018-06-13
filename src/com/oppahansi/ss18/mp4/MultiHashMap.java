308bb3d4a710b594b8cd779ba887975ab667e037
oppahansi <oppahansi@gmail.com>
2018-06-13 21:14:01 +0200

    MP4, ungeloest, nicht kompilierbar

---
+++ src/com/oppahansi/ss18/mp4/MultiHashMap.java
@@ -0,0 +1,19 @@
+/*
+ * Created by: Network Embedded Systems, Universität Duisburg-Essen
+ * Solved/Edited by Oppa Hansi. Possible solution - there are other ways to solve these tasks.
+ */
+
+package com.oppahansi.ss18.mp4;
+
+/**
+ * Implements a multimap using a hashmap that contains
+ * array lists to store its elements.
+ *
+ * @author Marcus
+ *
+ * @param <K> The key.
+ * @param <V> The value.
+ */
+public class MultiHashMap { // TODO: change hierarchy
+		
+}
---
+++ src/com/oppahansi/ss18/mp4/StringMapIO.java
@@ -0,0 +1,17 @@
+/*
+ * Created by: Network Embedded Systems, Universität Duisburg-Essen
+ * Solved/Edited by Oppa Hansi. Possible solution - there are other ways to solve these tasks.
+ */
+
+package com.oppahansi.ss18.mp4;
+
+/**
+ * A utility class that reads and writes multi maps containing strings
+ * to a file in the following format: key = value\newline
+ * 
+ * @author Marcus
+ */
+public class StringMapIO { // TODO: change hierachy and implement
+	
+	
+}
